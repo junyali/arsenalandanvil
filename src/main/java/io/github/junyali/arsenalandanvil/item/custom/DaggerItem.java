b4b8f1797d359568c7caf0557f95e32578f09a23
Junya <junjia.liu@pm.me>
2025-05-28 20:57:00 +0100

    Add dagger class

---
+++ src/main/java/io/github/junyali/arsenalandanvil/item/custom/DaggerItem.java
@@ -0,0 +1,22 @@
+package io.github.junyali.arsenalandanvil.item.custom;
+
+import io.github.junyali.arsenalandanvil.util.ItemAttributeBuilder;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+
+public class DaggerItem extends SwordItem {
+    public DaggerItem(Tier tier, Properties properties) {
+        super(tier, properties.attributes(createAttributes(tier)));
+    }
+
+    private static ItemAttributeModifiers createAttributes(Tier tier) {
+        return ItemAttributeBuilder.weapon()
+                .attackDamage(tier.getAttackDamageBonus() + 1.0f)
+                .attackSpeed(-1.8f)
+                .reach(-0.5f)
+                .critChance(0.15f)
+                .critDamage(0.1f)
+                .build();
+    }
+}
